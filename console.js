4750378c3a73284256faa7fd506f5760ef1a2f1f
Your Name <stiant@getacademy.no>
2023-10-24 00:57:10 +0200

    update console.js

--- console.js
+++ console.js
@@ -136,4 +136,4 @@ window.startTTT = function() {
 ;
 var t = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)
   , o = /Firefox/.test(navigator.userAgent);
-(t || o) && console.log("%cSource = vg.no\n\nLyst til å spille Tic Tac Toe?\nSkriv startTTT();", "font-weight:bold; font-size: 24px");
+(t || o) && console.log("%cSource = vg.no\n\nLyst til å spille Tic Tac Toe?\nSkriv startTTT();\n\nif (solo === true) {\nreturn doubleTab();\n}\n", "font-weight:bold; font-size: 24px");
